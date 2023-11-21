190bffa10244c128ab3fe33b378a63d0e49b1288
Daniel <daniel.ungurean2@gmail.com>
2023-11-21 16:38:39 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Exchange
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FromComboBox = new System.Windows.Forms.ComboBox();
+            this.ToComboBox = new System.Windows.Forms.ComboBox();
+            this.AmountTextBox = new System.Windows.Forms.TextBox();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.ExchangeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FromComboBox
+            // 
+            this.FromComboBox.FormattingEnabled = true;
+            this.FromComboBox.Location = new System.Drawing.Point(12, 12);
+            this.FromComboBox.Name = "FromComboBox";
+            this.FromComboBox.Size = new System.Drawing.Size(121, 21);
+            this.FromComboBox.TabIndex = 0;
+            // 
+            // ToComboBox
+            // 
+            this.ToComboBox.FormattingEnabled = true;
+            this.ToComboBox.Location = new System.Drawing.Point(12, 39);
+            this.ToComboBox.Name = "ToComboBox";
+            this.ToComboBox.Size = new System.Drawing.Size(121, 21);
+            this.ToComboBox.TabIndex = 1;
+            // 
+            // AmountTextBox
+            // 
+            this.AmountTextBox.Location = new System.Drawing.Point(139, 12);
+            this.AmountTextBox.Name = "AmountTextBox";
+            this.AmountTextBox.Size = new System.Drawing.Size(145, 20);
+            this.AmountTextBox.TabIndex = 2;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(314, 15);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 13);
+            this.ResultLabel.TabIndex = 3;
+            // 
+            // ExchangeButton
+            // 
+            this.ExchangeButton.Location = new System.Drawing.Point(139, 37);
+            this.ExchangeButton.Name = "ExchangeButton";
+            this.ExchangeButton.Size = new System.Drawing.Size(145, 23);
+            this.ExchangeButton.TabIndex = 4;
+            this.ExchangeButton.Text = "Exchange";
+            this.ExchangeButton.UseVisualStyleBackColor = true;
+            this.ExchangeButton.Click += new System.EventHandler(this.ExchangeButton_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 78);
+            this.Controls.Add(this.ExchangeButton);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.AmountTextBox);
+            this.Controls.Add(this.ToComboBox);
+            this.Controls.Add(this.FromComboBox);
+            this.Name = "Main";
+            this.Text = "Exchange";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox FromComboBox;
+        private System.Windows.Forms.ComboBox ToComboBox;
+        private System.Windows.Forms.TextBox AmountTextBox;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button ExchangeButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exchange
+{
+    public partial class Main : Form
+    {
+
+        private const string API_KEY = "b07996ddbb3a1b9bd96826ae";
+        private const string BASE_URL = "https://v6.exchangerate-api.com/v6/";
+
+        private Dictionary<string, string> _currencies;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            _currencies = GetCurrencies().Result;
+            foreach (var value in _currencies)
+            {
+                FromComboBox.Items.Add(value);
+                ToComboBox.Items.Add(value);
+            }
+            FromComboBox.ValueMember = "Key";
+            FromComboBox.DisplayMember = "Value";
+            ToComboBox.ValueMember = "Key";
+            ToComboBox.DisplayMember = "Value";
+        }
+
+        private async Task<Dictionary<string, string>> GetCurrencies()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(BASE_URL + API_KEY + "/codes").ConfigureAwait(false);
+
+                    response.EnsureSuccessStatusCode();
+
+                    string json = await response.Content.ReadAsStringAsync();
+                    JObject responseData = JObject.Parse(json);
+
+                    if (responseData["result"].ToString() != "success")
+                    {
+                        throw new InvalidOperationException("API request was not successful.");
+                    }
+
+                    JArray supportedCodesArray = responseData["supported_codes"] as JArray;
+
+                    Dictionary<string, string> currencyCodes = new Dictionary<string, string>();
+
+                    foreach (var codeInfo in supportedCodesArray)
+                    {
+                        string code = codeInfo[0].ToString();
+                        string name = codeInfo[1].ToString();
+                        currencyCodes.Add(code, name);
+                    }
+
+                    return currencyCodes;
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"HTTP request error: {ex.Message}");
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                    throw;
+                }
+            }
+        }
+
+        private async Task<double> GetExchangeRate(string baseCurrency, string targetCurrency)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(BASE_URL + API_KEY + "/pair/" + baseCurrency + "/" + targetCurrency).ConfigureAwait(false);
+
+                    response.EnsureSuccessStatusCode();
+
+                    string json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    JObject responseData = JObject.Parse(json);
+
+                    if (responseData["result"].ToString() != "success")
+                    {
+                        throw new InvalidOperationException("API request was not successful.");
+                    }
+
+                    double exchangeRate = Convert.ToDouble(responseData["conversion_rate"]);
+                    return exchangeRate;
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"HTTP request error: {ex.Message}");
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                    throw;
+                }
+            }
+        }
+
+        private void ExchangeButton_Click(object sender, EventArgs e)
+        {
+            if (FromComboBox.SelectedItem == null || ToComboBox.SelectedItem == null && string.IsNullOrWhiteSpace(AmountTextBox.Text)) { return; }
+
+            try {
+                string from = ((KeyValuePair<string, string>)FromComboBox.SelectedItem).Key;
+                string to = ((KeyValuePair<string, string>)ToComboBox.SelectedItem).Key;
+                ResultLabel.Text = (Convert.ToDouble(AmountTextBox.Text) * GetExchangeRate(from, to).Result).ToString();
+            } catch (Exception)
+            {
+                return;
+            }
+        }
+    }
+}
